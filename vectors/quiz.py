10159f843bb12c32f1098943ae90024ffed69c47
philiplessner <philiplessner@gmail.com>
2015-05-26 13:25:49 -0400

    Added segment and zeri_vec

--- vectors/quiz.py
+++ vectors/quiz.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from vec import Vec
 
 
 def addn(v, w):
@@ -7,3 +8,13 @@ def addn(v, w):
 
 def scalar_vect_mult(alpha, v):
     return [alpha * e for e in v]
+
+
+def segment(pt1, pt2):
+    return [addn(scalar_vect_mult(i / 100.,  pt1),
+                 scalar_vect_mult(1. - i / 100., pt2))
+            for i in range(0, 101)]
+
+
+def zero_vec(D):
+    return Vec(D, {d: 0 for d in D})
