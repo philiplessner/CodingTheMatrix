fdc6f71986a7a47113044c65e091bf8ebe25b622
philiplessner <philiplessner@gmail.com>
2015-09-27 18:00:54 -0400

    Lin_comb_vec_mat_mult added

--- matrix/mvm_problems.py
+++ matrix/mvm_problems.py
@@ -5,9 +5,33 @@ from matutil import listlist2mat
 
 
 def lin_comb_mat_vec_mult(M, v):
+    '''
+    >>> M = listlist2mat([[-1, 1, 2], [1, 2, 3], [2, 2, 1]])
+    >>> v = Vec({0, 1, 2}, {0: 1, 1: 2, 2: 0})
+    >>> lin_comb_mat_vec_mult(M, v) == Vec({0, 1, 2},{0: 1, 1: 5, 2: 6})
+    True
+    >>> A = Mat(({'a', 'b'}, {'@', '#', '?'}),
+    ... {('a', '@'): 2, ('a', '#'): 1, ('a', '?'): 3, ('b', '@'): 20,
+    ... ('b', '#'): 10, ('b', '?'): 30})
+    >>> b = Vec({'@', '#', '?'}, {'@': 0.5, '#': 5, '?': -1})
+    >>> lin_comb_mat_vec_mult(A, b) == Vec({'a', 'b'}, {'a': 3, 'b': 30.0})
+    True
+    '''
     assert v.D == M.D[1]
     rd = mat2rowdict(M)
-    return Vec(M.D[0], {k: rd[k] * v for k in v.D})
+    return Vec(M.D[0], {k: rd[k] * v for k in M.D[0]})
+
+
+def lin_comb_vec_mat_mult(v, M):
+    '''
+    >>> M2 = listlist2mat([[-5, 10], [-4, 8], [-3, 6], [-2, 4]])
+    >>> v3 = Vec({0, 1, 2, 3}, {0: 4, 1: 3, 2: 2, 3: 1})
+    >>> lin_comb_vec_mat_mult(v3, M2) == Vec({0, 1},{0: -40, 1: 80})
+    True
+    '''
+    assert v.D == M.D[0]
+    cd = mat2coldict(M)
+    return Vec(M.D[1], {k: v * cd[k] for k in M.D[1]})
 
 
 if __name__ == '__main__':
@@ -15,3 +39,6 @@ if __name__ == '__main__':
     M = listlist2mat(l)
     v = Vec({0, 1, 2}, {0: 1, 1: 2, 2: 0})
     v2 = lin_comb_mat_vec_mult(M, v)
+    M2 = listlist2mat([[-5, 10], [-4, 8], [-3, 6], [-2, 4]])
+    v3 = Vec({0, 1, 2, 3}, {0: 4, 1: 3, 2: 2, 3: 1})
+    v4 = lin_comb_vec_mat_mult(v3, M2)
