4a4b62d08e46b24bf5b07d26cae4cb7a93a46ee4
philiplessner <philiplessner@gmail.com>
2015-09-28 21:18:37 -0400

    Changed procedures to reflect dot product and not linear comb implementation

--- matrix/mvm_problems.py
+++ matrix/mvm_problems.py
@@ -4,17 +4,17 @@ from matutil import mat2rowdict, mat2coldict, rowdict2mat, coldict2mat
 from matutil import listlist2mat
 
 
-def lin_comb_mat_vec_mult(M, v):
+def dot_product_mat_vec_mult(M, v):
     '''
     >>> M = listlist2mat([[-1, 1, 2], [1, 2, 3], [2, 2, 1]])
     >>> v = Vec({0, 1, 2}, {0: 1, 1: 2, 2: 0})
-    >>> lin_comb_mat_vec_mult(M, v) == Vec({0, 1, 2},{0: 1, 1: 5, 2: 6})
+    >>> dot_product_mat_vec_mult(M, v) == Vec({0, 1, 2},{0: 1, 1: 5, 2: 6})
     True
     >>> A = Mat(({'a', 'b'}, {'@', '#', '?'}),
     ... {('a', '@'): 2, ('a', '#'): 1, ('a', '?'): 3, ('b', '@'): 20,
     ... ('b', '#'): 10, ('b', '?'): 30})
     >>> b = Vec({'@', '#', '?'}, {'@': 0.5, '#': 5, '?': -1})
-    >>> lin_comb_mat_vec_mult(A, b) == Vec({'a', 'b'}, {'a': 3, 'b': 30.0})
+    >>> dot_product_mat_vec_mult(A, b) == Vec({'a', 'b'}, {'a': 3, 'b': 30.0})
     True
     '''
     assert v.D == M.D[1]
@@ -22,11 +22,11 @@ def lin_comb_mat_vec_mult(M, v):
     return Vec(M.D[0], {k: rd[k] * v for k in M.D[0]})
 
 
-def lin_comb_vec_mat_mult(v, M):
+def dot_product_vec_mat_mult(v, M):
     '''
     >>> M2 = listlist2mat([[-5, 10], [-4, 8], [-3, 6], [-2, 4]])
     >>> v3 = Vec({0, 1, 2, 3}, {0: 4, 1: 3, 2: 2, 3: 1})
-    >>> lin_comb_vec_mat_mult(v3, M2) == Vec({0, 1},{0: -40, 1: 80})
+    >>> dot_product_vec_mat_mult(v3, M2) == Vec({0, 1},{0: -40, 1: 80})
     True
     '''
     assert v.D == M.D[0]
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     l = [[-1, 1, 2], [1, 2, 3], [2, 2, 1]]
     M = listlist2mat(l)
     v = Vec({0, 1, 2}, {0: 1, 1: 2, 2: 0})
-    v2 = lin_comb_mat_vec_mult(M, v)
+    v2 = dot_product_mat_vec_mult(M, v)
     M2 = listlist2mat([[-5, 10], [-4, 8], [-3, 6], [-2, 4]])
     v3 = Vec({0, 1, 2, 3}, {0: 4, 1: 3, 2: 2, 3: 1})
-    v4 = lin_comb_vec_mat_mult(v3, M2)
+    v4 = dot_product_vec_mat_mult(v3, M2)
